a224c1523af220a4cb456f0f2e0fb929180fde3c
ratovia <ratovia@hotmail.co.jp>
2020-09-11 15:04:37 +0900

    update!

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,7 @@ require("youtube_slick")
 
 require("japan-heatmap")
 require("sidebar-covid-card")
+require("hello")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
