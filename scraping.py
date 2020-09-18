cffab93ff22866523e3b83c03b93254d107e9532
ratovia <ratovia@hotmail.co.jp>
2020-09-18 15:21:22 +0900

    update! image

--- scraping.py
+++ scraping.py
@@ -5,8 +5,14 @@ import MySQLdb
 import time
 from datetime import *
 import os
-
-
+import sys
+from selenium import webdriver
+import chromedriver_binary
+from selenium.webdriver import Chrome,ChromeOptions
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import datetime
+# import chromedriver
 # 接続する 
 connect = MySQLdb.connect(host='localhost', port=3306, user='root', passwd='' , db='covid19_curation_development', charset='utf8')
 # カーソルを取得する
@@ -16,6 +22,9 @@ cur = connect.cursor()
 sql = "select * from articles"
 cur.execute(sql)
 
+options = ChromeOptions()
+options.add_argument('--headless')
+driver = Chrome(options=options)
 
 baseURL = "https://www.google.com/search?q=%E3%82%B3%E3%83%AD%E3%83%8A&rlz=1C5CHFA_enJP853JP854&source=lnms&sa=X&ved=0ahUKEwiw3YHWw6vrAhWUxosBHSaNAo8Q_AUIDSgA&biw=1440&bih=821&dpr=2"
 requestURL = requests.get(baseURL)
@@ -28,20 +37,19 @@ images = soup.find_all("")
 for title in titles:
     print(title)
 
-for dom in doms[:1]:
+for dom in doms[:10]:
     href = re.split('&', dom.get('href'))
     siteBaseURL = re.split('q=', href[0])[1]
-    siteRequestURL = requests.get(siteBaseURL)
-    soup = BeautifulSoup(siteRequestURL.content, "html.parser")
-    topImgTag = soup.find_all("img")[0]
-    print(topImgTag)
-    img = topImgTag.get('src')
-    req = requests.get(img)
-    print(req.content)
+    current_time = datetime.datetime.today()
 
-    with open('/Users/tech-camp/workspace/covid19_curation/public/images/scraping/' + target.split('/')[-1], 'wb') as f:
-        f.write(req.content)
+    filename = os.path.join('/Users/tech-camp/workspace/covid19_curation/xxx/xxx/xxx', "screen{}.png".format(current_time.strftime("%Y%m%d%H%M%S")))
+    driver.get(siteBaseURL)
+    w = driver.execute_script("return document.body.scrollWidth;")
+    h = 500
+    driver.set_window_size(w,h)
+    driver.save_screenshot(filename)
         
+driver.quit()
 connect.commit()
 cur.close
 connect.close
